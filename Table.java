70cb98741e0fec2975b32c34db0c14cbf78e0531
sallydabbah <sallydabbah@microsoft.com>
2024-02-11 20:52:17 +0200

    added new cons in table

--- Table.java
+++ Table.java
@@ -3,11 +3,23 @@ import java.util.List;
 
 public class Table {
     int tableNumber;
+    Boolean isOccupied;
     List<OrderItem> orderItems;
-
+    
     public Table(int tableNumber) {
         this.tableNumber = tableNumber;
         this.orderItems = new ArrayList<>();
+    }
+
+    public Table(int tableNumber,int isOccupied) {
+        this.tableNumber = tableNumber;
+        this.orderItems = new ArrayList<>();
+        if(isOccupied == 0){
+            this.isOccupied = false;
+        }
+        else {
+            this.isOccupied = true;
+        }      
     }
 
     public void addOrderItem(OrderItem orderItem) {
